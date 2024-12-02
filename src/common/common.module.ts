f5ddca88846a2153f0d8a270acaa90f55f81c98e
agm9581 <agm9581@gmail.com>
2024-12-02 16:55:13 +0100

    Transform into a instance of the sent DTO

--- src/common/common.module.ts
+++ src/common/common.module.ts
@@ -2,6 +2,15 @@ import { Module, ValidationPipe } from "@nestjs/common";
 import { APP_PIPE } from "@nestjs/core";
 
 @Module({
-  providers: [{ provide: APP_PIPE, useValue: new ValidationPipe() }],
+  providers: [
+    {
+      provide: APP_PIPE,
+      useValue: new ValidationPipe({
+        whitelist: true,
+        forbidNonWhitelisted: true,
+        transform: true,
+      }),
+    },
+  ],
 })
 export class CommonModule {}
