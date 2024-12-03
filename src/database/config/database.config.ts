d4b8847240c6e3ec3d7840583935dcf4a67f32e6
agm9581 <agm9581@gmail.com>
2024-12-03 18:38:38 +0100

    Refactor database config

--- src/database/config/database.config.ts
+++ src/database/config/database.config.ts
@@ -1,5 +1,11 @@
+import { TypeOrmModuleOptions } from "./../../../node_modules/@nestjs/typeorm/dist/interfaces/typeorm-options.interface.d";
 import { registerAs } from "@nestjs/config";
 
-export default registerAs("database", () => ({
-  url: process.env.DATASOURCE_URL,
-}));
+export default registerAs("database", () => {
+  const config = {
+    type: "postgres",
+    url: process.env.DATASOURCE_URL,
+    autoLoadEntities: true,
+  } as const satisfies TypeOrmModuleOptions;
+  return config;
+});
--- src/database/database.module.ts
+++ src/database/database.module.ts
@@ -4,18 +4,6 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import databaseConfig from "./config/database.config";
 
 @Module({
-  imports: [
-    TypeOrmModule.forRootAsync({
-      imports: [ConfigModule.forFeature(databaseConfig)],
-      inject: [databaseConfig.KEY],
-      useFactory: (
-        databaseConfiguration: ConfigType<typeof databaseConfig>,
-      ) => ({
-        type: "postgres",
-        url: databaseConfiguration.url,
-        autoLoadEntities: true,
-      }),
-    }),
-  ],
+  imports: [TypeOrmModule.forRootAsync(databaseConfig.asProvider())],
 })
 export class DatabaseModule {}
