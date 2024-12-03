d41cfeac800a3e04c16e48a834faf65d1dc7b6d1
agm9581 <agm9581@gmail.com>
2024-12-03 18:13:35 +0100

    Data source now uses composed url variables with pg parameters

--- src/database/data-source.ts
+++ src/database/data-source.ts
@@ -1,17 +1,13 @@
 import { DataSource } from "typeorm";
+import * as dotenv from 'dotenv'
 
 export default new DataSource({
   type: "postgres",
-  username: "postgres",
-  password: "password",
-  host: "localhost",
-  port: 5432,
-  database: "postgres",
+  url: process.env.DATASOURCE_URL,
   entities: ["dist/**/*.entity.js"],
   migrations: ["dist/database/migrations/*.js"],
 });
 
-
 //Command to generate a migrations
 
-  //npm build && npm typeorm migration:generate -d dist/database/data-source -p src/database/migrations/create-user
+//npm build && npm typeorm migration:generate -d dist/database/data-source -p src/database/migrations/create-user
