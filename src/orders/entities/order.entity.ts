9105862c40191e15815b071c3c0a4ae108e45586
antonio <agm9581@gmail.com>
2024-12-07 18:40:13 +0100

    Relation between user and order

--- src/orders/entities/order.entity.ts
+++ src/orders/entities/order.entity.ts
@@ -1,16 +1,18 @@
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Column, Entity, ManyToMany, PrimaryGeneratedColumn } from "typeorm";
 import { OrderStatus } from "../enums/order-status.enum";
 import { RegistryDates } from "src/common/embedded/registry-dates.embedded";
+import { User } from "src/users/entities/user.entity";
 
 @Entity()
 export class Order {
     @PrimaryGeneratedColumn()
     id:number
-
-    @Column()
+    @Column({type: 'enum', enum: OrderStatus, default: OrderStatus.AWAITING_PAYMENT})
     status:OrderStatus
 
-    @Column()
+    @Column(()=>  RegistryDates, {prefix: false})
     registryDates: RegistryDates
 
+    @ManyToMany(()=> User, (customer) => customer.orders, {nullable:false })
+    customer:User
 }
--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,5 +1,6 @@
 import { RegistryDates } from "src/common/embedded/registry-dates.embedded";
-import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+import { Order } from "src/orders/entities/order.entity";
+import { Column, Entity, OneToMany, PrimaryGeneratedColumn } from "typeorm";
 
 @Entity()
 export class User {
@@ -21,4 +22,7 @@ export class User {
   @Column(() => RegistryDates, { prefix: false })
   //Embedded entity not a primitive type, anonymous function returning the type
   registryDates: RegistryDates;
+
+  @OneToMany(()=> Order, (order) => order.customer)
+  orders: Order[]
 }
