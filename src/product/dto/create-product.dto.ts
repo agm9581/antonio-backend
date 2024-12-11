876e53a6f5cba761e0db874d97a9637200b5257f
antonio <agm9581@gmail.com>
2024-12-11 10:45:01 +0100

    Implement product logic

--- src/product/dto/create-product.dto.ts
+++ src/product/dto/create-product.dto.ts
@@ -1,1 +1,23 @@
-export class CreateProductDto {}
+import { Type } from "class-transformer";
+import { ArrayNotEmpty, IsCurrency, IsNumber, IsOptional, IsPositive, Length, ValidateNested } from "class-validator";
+import { IsCardinal } from "src/common/decorators/is-cardinal.decorator";
+import { IsEntity } from "src/common/decorators/is-entity.decorator";
+import { IdDto } from "src/common/dto/id.dto";
+
+export class CreateProductDto {
+
+
+    @Length(2, 50)
+    readonly name: string;
+
+    @IsOptional()
+    @Length(1, 500)
+    readonly description:string
+
+    @IsCurrency()
+    readonly price:number
+
+    @ArrayNotEmpty()
+    @IsEntity()
+    readonly categories: IdDto[]
+}
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,27 +1,49 @@
-import { Injectable } from "@nestjs/common";
+import { Injectable, NotFoundException } from "@nestjs/common";
 import { CreateProductDto } from "./dto/create-product.dto";
 import { UpdateProductDto } from "./dto/update-product.dto";
+import { InjectRepository } from "@nestjs/typeorm";
+import { Product } from "./entities/product.entity";
+import { Repository } from "typeorm";
 import { PaginationDto } from "src/common/dto/pagination.dto";
+import { DEFAULT_PAGE_SIZE } from "src/common/util/common.constants";
 
 @Injectable()
-export class ProductService {
+export class ProductsService {
+  constructor(
+    @InjectRepository(Product)
+    private readonly productsRepository: Repository<Product>,
+  ) {}
   create(createProductDto: CreateProductDto) {
-    return "This action adds a new product";
+    const product = this.productsRepository.create(createProductDto);
+    return this.productsRepository.save(product);
   }
 
-  findAll(paginationDto: PaginationDto) {
-    return `This action returns all product`;
+  findAll(paginatioDto: PaginationDto) {
+    const { limit, offset } = paginatioDto;
+    return this.productsRepository.find({
+      skip: offset,
+      take: limit ?? DEFAULT_PAGE_SIZE.PRODUCT,
+    });
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} product`;
+  async findOne(id: number) {
+    const product = await this.productsRepository.findOne({ where:{id}, relations:{orders:true}});
+    if (!product) {
+      throw new NotFoundException("Product not found");
+    }
+    return product;
   }
 
-  update(id: number, updateProductDto: UpdateProductDto) {
-    return `This action updates a #${id} product`;
+  async update(id: number, updateProductDto: UpdateProductDto) {
+    const product = await this.productsRepository.preload({ id, ...updateProductDto });
+    if (!product) {
+      throw new NotFoundException("Product not found");
+    }
+    return this.productsRepository.save(product);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} product`;
+  async remove(id: number) {
+    const product = await this.findOne(id);
+    return this.productsRepository.remove(product);
   }
 }
