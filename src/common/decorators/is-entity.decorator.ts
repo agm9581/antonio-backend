6f785e808489a09754a4f6e2bb73726cf3574cf8
antonio <agm9581@gmail.com>
2024-12-11 10:44:44 +0100

    Validate object with a unique field of type number

---
+++ src/common/decorators/is-entity.decorator.ts
@@ -0,0 +1,12 @@
+import { applyDecorators } from "@nestjs/common";
+import { Type } from "class-transformer";
+import { ArrayNotEmpty, IsInt, IsPositive, ValidateNested, ValidationOptions } from "class-validator";
+import { IdDto } from "../dto/id.dto";
+
+/**
+ * Check if the value is an object with only a serial id
+ */
+export const IsEntity = (
+  validationOptions?: ValidationOptions,
+): PropertyDecorator =>
+  applyDecorators( ValidateNested(validationOptions), Type(()=> IdDto));
