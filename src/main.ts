6edfe97d69a3faf54153bcb64a109635d012712c
antonio <agm9581@gmail.com>
2025-01-04 10:50:19 +0100

    Install swagger and expose a swagger document for specification

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import helmet from 'helmet';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors()
   app.use(helmet())
+  const config = new DocumentBuilder()
+    .setTitle('The antonio shop')
+    .setDescription('Documentation for the shop API')
+    .setVersion('1.0')
+    .build()
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('api', app, document)
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
