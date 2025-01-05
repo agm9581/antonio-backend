6a77e72636806e954a04ae198a459e09ebcf1ae9
antonio <agm9581@gmail.com>
2025-01-05 10:52:39 +0100

    Checking file signatures for possible malicious tampered files

--- src/files/util/file-validation.util.ts
+++ src/files/util/file-validation.util.ts
@@ -2,6 +2,7 @@ import { FileTypeValidator, FileValidator, MaxFileSizeValidator } from "@nestjs/
 import bytes from "bytes"
 import { lookup } from "mime-types"
 import { NonEmptyArray } from "src/common/util/array.util"
+import { FileSignatureValidator } from "../validators/file-signature.validators"
 type FileSize = `${number}${'KB' | 'MB' | 'GB'}`
 type FileType = 'png' | 'jpeg' | 'pdf'
 
@@ -13,5 +14,5 @@ const createFileTypeRegex = (fileTypes: FileType[]) => {
 
 export const createFileValidators = (maxSize: FileSize, ...fileTypes: NonEmptyArray<FileType>): FileValidator[] => {
     const fileTypeRegex = createFileTypeRegex(fileTypes)
-    return [new MaxFileSizeValidator({ maxSize: bytes(maxSize) }), new FileTypeValidator({ fileType: fileTypeRegex })]
+    return [new MaxFileSizeValidator({ maxSize: bytes(maxSize) }), new FileTypeValidator({ fileType: fileTypeRegex }), new FileSignatureValidator()]
 }
