d86afb22805fbabd92f524709945eca60301fdc1
antonio <agm9581@gmail.com>
2025-01-05 10:40:15 +0100

    Validating media types instead of extensions

--- src/files/util/file-validation.util.ts
+++ src/files/util/file-validation.util.ts
@@ -1,10 +1,17 @@
 import { FileTypeValidator, FileValidator, MaxFileSizeValidator } from "@nestjs/common"
 import bytes from "bytes"
+import { lookup } from "mime-types"
 import { NonEmptyArray } from "src/common/util/array.util"
 type FileSize = `${number}${'KB' | 'MB' | 'GB'}`
 type FileType = 'png' | 'jpeg' | 'pdf'
 
+
+const createFileTypeRegex = (fileTypes: FileType[]) => {
+    const mediaTypes = fileTypes.map(type => lookup(type))
+    return new RegExp(mediaTypes.join('|'))
+}
+
 export const createFileValidators = (maxSize: FileSize, ...fileTypes: NonEmptyArray<FileType>): FileValidator[] => {
-    const fileTypeRegex = new RegExp(fileTypes.join('|'))
+    const fileTypeRegex = createFileTypeRegex(fileTypes)
     return [new MaxFileSizeValidator({ maxSize: bytes(maxSize) }), new FileTypeValidator({ fileType: fileTypeRegex })]
 }
