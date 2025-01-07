6aa96bc80d11a076271cbbc40ee3452cd281b7e8
antonio <agm9581@gmail.com>
2025-01-07 23:21:52 +0100

    Interceptor to extract body from multipart form data

---
+++ src/files/interceptors/body/body.interceptor.ts
@@ -0,0 +1,19 @@
+import { BadRequestException, CallHandler, ExecutionContext, Injectable, NestInterceptor } from '@nestjs/common';
+import { Request } from 'express';
+import { Observable } from 'rxjs';
+
+@Injectable()
+export class BodyInterceptor implements NestInterceptor {
+  intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
+    const request = context.switchToHttp().getRequest<Request>();
+
+    try {
+      const body = JSON.parse(request.body.body)
+      request.body = body
+    } catch (error) {
+      throw new BadRequestException(error.message)
+    }
+
+    return next.handle();
+  }
+}
