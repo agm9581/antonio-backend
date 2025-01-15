d8371d372f00c0027ae7b014bc91db0fb5ee0ccc
antonio <agm9581@gmail.com>
2025-01-16 00:27:19 +0100

    Sending page and max values directly in pagination

--- src/querying/dto/pagination.dto.ts
+++ src/querying/dto/pagination.dto.ts
@@ -1,13 +1,16 @@
-import { IsInt, IsOptional, IsPositive } from "class-validator";
+import { IsInt, IsOptional, IsPositive, Max } from "class-validator";
 import { IsCardinal } from "src/common/decorators/validators/is-cardinal.decorator";
+import { MAX_PAGE_NUMBER, MAX_PAGE_SIZE } from "../util/querying.constants";
 
 
 export class PaginationDto {
   @IsOptional()
+  @Max(MAX_PAGE_SIZE)
   @IsCardinal()
-  limit?: number;
+  readonly limit?: number;
 
   @IsOptional()
+  @Max(MAX_PAGE_NUMBER)
   @IsCardinal()
-  offset?: number;
+  readonly page?: number = 1;
 }
--- src/querying/util/querying.constants.ts
+++ src/querying/util/querying.constants.ts
@@ -1,4 +1,6 @@
 
+export const MAX_PAGE_SIZE = 100;
+export const MAX_PAGE_NUMBER = 25
 export const DEFAULT_PAGE_SIZE = {
     USER: 10,
     ORDER: 5,
